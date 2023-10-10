3f7ab79c4b41274d0a8edc0186b3a114c02b12c6
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-10 20:39:55 +0200

    input const

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// const kmPrice = 0.21;
+// const discountUnder18 = 20/100;
+// const discountOver65 = 20/100;
+// price = kmPrice * km;
+
+const nome = document.getElementById('name');
+console.log(nome);
+const distanza = document.getElementById('km');
+console.log(distanza);
+const età = document.getElementById('età')
+console.log(età);
