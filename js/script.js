d65d09f5f2055d7b4a0e9b897b859fe0140491bf
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-11 22:56:07 +0200

    button + event (need test)

--- js/script.js
+++ js/script.js
@@ -15,4 +15,22 @@ reverseButton.addEventListener('click', function(){
     ticketEl.classList.add('d-none');
 });
 
+button.addEventListener('click', function(){
+    errorEl.classList.add('d-none');
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const km = parseInt(document.getElementById('km').value);
+    let offerType;
+    let price = kmPrice * km;
+    if (age === 'under18'){
+        offerType = `Discount ${disount18}&percnt;`;
+        price -= price * discount18/100;
+    }   else if(age === 'over65'){
+        offerType = `Discount ${discount65}&percnt;`;
+        price -= price * discount65/100;
+    }   else {
+        offerType = 'No discount';
+    }
+})
+
 
