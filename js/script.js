bbf65d638b4877cd934177df733c0e5ce7e4ed74
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-11 22:02:51 +0200

    undo

--- js/script.js
+++ js/script.js
@@ -7,27 +7,6 @@
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 
-const button = document.querySelector('#btnStart');
-console.log(button)
 
-button.addEventListener('click', function(){
-    let nome = document.getElementById('nome').value;
-    let distanza = document.getElementById('km').value;
-    let età = document.getElementById('età');
-
-    if (nome === '' || distanza === ''){
-        alert('Inserisci i tuoi dati')
-    } else{
-        // console.log(nome);
-        // console.log(distanza);
-        // console.log(minorenne);
-        // console.log(maggiorenne);
-
-        const ticket = document.querySelector('#ticket');
-        ticket.innerHTML = ` ${nome} ${distanza} ${età}`;
-        ticket.classList.remove('d-none');
-        console.log(ticket);
-      }
-});
 
 
